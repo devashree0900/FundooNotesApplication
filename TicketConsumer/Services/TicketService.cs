5335236d72819f694d282d3148a5628f13aa7468
devashree0900 <85434516+devashree0900@users.noreply.github.com>
2023-02-14 17:51:30 +0530

    Add files via upload

---
+++ TicketConsumer/Services/TicketService.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Sockets;
+using System.Threading.Tasks;
+using MassTransit;
+using TicketConsumer.Model;
+
+namespace TicketConsumer.Services
+{
+    public class TicketService: IConsumer<UserTicket>
+    {
+        public async Task Consume(ConsumeContext<UserTicket> context)
+        {
+            var data = context.Message;
+            //Validate the Ticket Data
+            //Store to Database
+            //Notify the user via Email / SMS
+        }
+    }
+}
